60a7ac00f4b2435519ae9f6a61c9ebcd2cf0279c
dtreadwell <treadwell.d@northeastern.edu>
2025-02-05 21:09:38 -0800

    LAB 4 - Add new tests that fail for logical (3) and syntax (1) reasons

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,6 +5,97 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
+    # ~~~~~
+    # DAVID's TESTS. 1-3 fail for logical reasons, 4 fails because of syntax
+    # 1. LOGICAL - Test that "conjured" items degrade in Quality twice as fast as normal items
+    def test_conjure_grade_double_speed(self):
+        # Set quality default
+        sell_in_default = 5
+        quality_default = 50
+
+        # Create the items to test and instantiate a Gilded Rose object
+        items = [Item("Conjured", sell_in_default, quality_default), Item("Random", sell_in_default, quality_default)]
+        gilded_rose = GildedRose(items)
+
+        # Update the quality and grab the new items
+        gilded_rose.update_quality()
+        conjured_item = items[0]
+        random_item = items[1]
+
+        # Get the difference in both items compared to their original value
+        difference_conjured = quality_default - conjured_item.quality
+        difference_random = quality_default - random_item.quality
+
+        # Test the difference in conjured quality is double the difference in random item quality
+        self.assertEquals(difference_conjured, difference_random * 2)
+
+    # 2. LOGICAL - Test that "Backstage passes" increase in quality by when 10 days or less until concert date
+    # Assumes "sell-by" date is the same as the concert date
+    def test_backstage_passes_increase_quality_ten_days(self):
+        # Default values to test with. Testing days 6-10 because quality should double during these days
+        first_day = 6
+        last_day = 10
+        sell_in_days = [day for day in range(first_day, last_day + 1)]
+        quality_default = 50
+        expected_difference = 2
+
+        # Create items to test each day
+        items = []
+        for day in sell_in_days:
+            items.append(Item("Backstage pass", day, quality_default))
+
+        # Instantiate GildedRose and update quality
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+
+        # Check that all items have increased by two
+        differences = [item.quality - quality_default for item in items]
+        for difference in differences:
+            self.assertEquals(difference, expected_difference)
+
+    # 3. LOGICAL - Test that "Backstage passes" have a value of zero no matter what their quality is once sell in days
+    # goes below zero (the concert has passed)
+    def test_backstage_passes_quality_zero_after_concert(self):
+        # Set default values to test with to ensure multiple values go to zero
+        quality_one = 50
+        quality_two = 8
+        sell_in_default = -1  # Assumes 0 is day of concert
+        new_quality = 0
+
+        # Create the test items
+        items = [Item("Backstage pass", sell_in_default, quality_one), Item("Backstage pass", sell_in_default,
+                                                                            quality_two)]
+
+        # Instantiate GildedRose and update quality
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+
+        # Check both have a quality of 0
+        for item in items:
+            self.assertEquals(item.quality, new_quality)
+
+    # 4. SYNTAX  - Check that the system updates the SellIn value for an item; this method is not yet implemented
+    def test_sell_in_updates(self):
+        # Set default values
+        default_sell_in = 5
+        new_sell_in = 4
+        default_quality = 50
+
+        # Create the test object
+        items = [Item("Aged Brie", default_sell_in, default_quality)]
+
+        # Instantiate GildedRose and update quality
+        gilded_rose = GildedRose(items)
+
+        # Apply the method to update SellIn values
+        gilded_rose.update_sell_in()
+
+        # Check that the new sell in value is correct
+        self.assertEquals(items[0].quality, new_sell_in)
+
+    # END DAVID'S TESTS
+    # ~~~~~~
+
     # example of test that checks for logical errors
     def test_sulfuras_should_not_decrease_quality(self):
         items = [Item("Sulfuras", 5, 80)]
